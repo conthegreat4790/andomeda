b3b80abc90d412c6b744edce062d4fe58584088c
Ocrpea5 <github@me.nathanaldenjr.com>
2026-08-04 20:49:48 -0500

    Menu edit

--- Andromeda/Assets/Scripts/MainMenu.cs
+++ Andromeda/Assets/Scripts/MainMenu.cs
@@ -1,37 +1,125 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject pauseMenu;
+    [FormerlySerializedAs("pauseMenu")]
+    public GameObject pausePanel;
+    public GameObject optionsPanel;
     public bool isMenuActive;
     public AudioSource menuOpenCloseSound;
+    public Button optionsButton;
+    public Button exitButton;
 
-    void Start()
+    private void Awake()
     {
-        isMenuActive = false;
-        pauseMenu.SetActive(false);
+        if (optionsButton != null)
+        {
+            optionsButton.onClick.AddListener(OpenOptions);
+        }
     }
 
-    void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            isMenuActive = !isMenuActive;
+        ShowPausePanel(false);
+    }
 
-            if (isMenuActive)
-            {
-                CursorLockManager.UnlockCursor();
-                pauseMenu.SetActive(true);
-            }
-            else
-            {
-                CursorLockManager.LockCursor();
-                pauseMenu.SetActive(false);
-            }
+    private void OnDestroy()
+    {
+        if (optionsButton != null)
+        {
+            optionsButton.onClick.RemoveListener(OpenOptions);
+        }
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Escape))
+        {
+            return;
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (optionsPanel != null && optionsPanel.activeSelf)
+        {
+            ShowPausePanel();
+        }
+        else
+        {
+            TogglePausePanel();
+        }
+
+        PlayMenuSound();
+    }
+
+    public void OpenOptions()
+    {
+        if (!isMenuActive || optionsPanel == null)
+        {
+            return;
+        }
+
+        if (pausePanel != null)
+        {
+            pausePanel.SetActive(false);
+        }
+
+        optionsPanel.SetActive(true);
+        CursorLockManager.UnlockCursor();
+    }
+
+    public void ShowPausePanel(bool playSound = true)
+    {
+        if (pausePanel != null)
+        {
+            pausePanel.SetActive(true);
+        }
+
+        if (optionsPanel != null)
+        {
+            optionsPanel.SetActive(false);
+        }
+
+        isMenuActive = true;
+        CursorLockManager.UnlockCursor();
+
+        if (playSound)
+        {
+            PlayMenuSound();
+        }
+    }
+
+    public void CloseMenus()
+    {
+        if (pausePanel != null)
+        {
+            pausePanel.SetActive(false);
+        }
+
+        if (optionsPanel != null)
+        {
+            optionsPanel.SetActive(false);
+        }
+
+        isMenuActive = false;
+        CursorLockManager.LockCursor();
+    }
+
+    public void TogglePausePanel()
+    {
+        if (isMenuActive)
+        {
+            CloseMenus();
+        }
+        else
+        {
+            ShowPausePanel();
+        }
+    }
+
+    private void PlayMenuSound()
+    {
+        if (menuOpenCloseSound != null && menuOpenCloseSound.clip != null)
         {
             menuOpenCloseSound.PlayOneShot(menuOpenCloseSound.clip);
         }
