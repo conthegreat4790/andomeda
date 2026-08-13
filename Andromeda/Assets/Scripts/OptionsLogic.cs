d43199baa472b37fda484ed2ade223722790173c
Ocrpea5 <github@me.nathanaldenjr.com>
2026-08-13 17:00:53 -0500

    MinorTweaks

--- Andromeda/Assets/Scripts/OptionsLogic.cs
+++ Andromeda/Assets/Scripts/OptionsLogic.cs
@@ -4,10 +4,20 @@ public class OptionsLogic : MonoBehaviour
 {
     public Slider volumeSlider;
     public float currentVolume;
+    public Button resetToDefault;
 
+    void Start()
+    {
+        resetToDefault.onClick.AddListener(ResetToDefault);
+    }
     void Update()
     {
         AudioListener.volume = currentVolume / 100f;
         currentVolume = volumeSlider.value;
+    }
+    void ResetToDefault()
+    {
+        currentVolume = 50f;
+        volumeSlider.value = currentVolume;
     }
 }
