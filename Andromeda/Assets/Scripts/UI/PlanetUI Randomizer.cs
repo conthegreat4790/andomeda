21f9ce443865f3b5cece00430932105b7389e6bf
conthegreat4790 <conthegreat4790@gmail.com>
2026-09-05 18:49:47 -0700

    Update PlanetUI Randomizer.cs

--- Andromeda/Assets/Scripts/UI/PlanetUI Randomizer.cs
+++ Andromeda/Assets/Scripts/UI/PlanetUI Randomizer.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 public class PlanetUIRandomizer : MonoBehaviour
 {
     public List<Texture> possibleIconPool;
+    public Texture2D deadPlanetIcon;
+    public Texture2D hotPlanetIcon;
+    public Texture2D habitablePlanetIcon;
 
     // Update is called once per frame
     void Update()
